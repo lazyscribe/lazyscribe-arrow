e744ccb652f414087eac7abbcc430e87b03290ae
Akshay Gupta <akgcodes@gmail.com>
2025-09-30 04:02:00 +0000

    feat: adding the tests field to the output table schema

--- lazyscribe_arrow/interchange.py
+++ lazyscribe_arrow/interchange.py
@@ -1,10 +1,54 @@
 """Define methods for generating a PyArrow table from a project and/or repository."""
 
 from functools import singledispatch
+from typing import Any, Protocol
 
 import pyarrow as pa
 from lazyscribe import Project
 from slugify import slugify
+
+
+class HasMetricsParameters(Protocol):
+    """Class that has a parameters iterable."""
+
+    metrics: dict[str, int | float]
+    parameters: dict[str, Any]
+
+
+def _gen_metric_parameters_fields(
+    obj: HasMetricsParameters, fields: dict, field_map: dict[str, str]
+) -> tuple[dict, dict]:
+    """Generate the schema for parameters and/or metrics.
+
+    Parameters
+    ----------
+    obj : list[HasMetricsParameters]
+        A list of objects that has metrics and parameters.
+    fields : dict
+        The existing schema object.
+    field_map : dict
+        A dictionary mapping the field slug name to the attribute name in the
+        source object.
+
+    Returns
+    -------
+    dict
+        The updated ``fields`` dictionary.
+    dict
+        The field map.
+    """
+    for param_, value_ in obj.parameters.items():
+        if (param_slug_ := slugify(f"parameter-{param_}")) in fields:
+            continue
+        field_map[param_slug_] = param_
+        fields[param_slug_] = pa.scalar(value_).type
+    for metric_, value_ in obj.metrics.items():
+        if (metric_slug_ := slugify(f"metric-{metric_}")) in fields:
+            continue
+        field_map[metric_slug_] = metric_
+        fields[metric_slug_] = pa.scalar(value_).type
+
+    return fields, field_map
 
 
 @singledispatch
@@ -48,19 +92,19 @@ def _(obj: Project, /) -> pa.Table:
         "last_updated_by": pa.string(),
         "tags": pa.list_view(pa.string()),
     }
+    test_fields = {"name": pa.string(), "description": pa.string()}
+
     # Look for the superset of available parameters and metrics
     field_map_: dict[str, str] = {}
+    test_field_map_: dict[str, str] = {}
     for exp in obj.experiments:
-        for param_, value_ in exp.parameters.items():
-            if (param_slug_ := slugify(f"parameter-{param_}")) in fields:
-                continue
-            field_map_[param_slug_] = param_
-            fields[param_slug_] = pa.scalar(value_).type
-        for metric_, value_ in exp.metrics.items():
-            if (metric_slug_ := slugify(f"metric-{metric_}")) in fields:
-                continue
-            field_map_[metric_slug_] = metric_
-            fields[metric_slug_] = pa.scalar(value_).type
+        fields, field_map_ = _gen_metric_parameters_fields(exp, fields, field_map_)
+        # Look at the tests
+        for test_ in exp.tests:
+            test_fields, test_field_map_ = _gen_metric_parameters_fields(
+                test_, test_fields, test_field_map_
+            )
+    fields["tests"] = pa.list_view(pa.struct(test_fields))
 
     schema = pa.schema(fields)
     batches: list[pa.RecordBatch] = []
@@ -75,13 +119,33 @@ def _(obj: Project, /) -> pa.Table:
             "last_updated_by": [exp.last_updated_by],
             "tags": [exp.tags],
         }
+
         for field in schema:
             if field.name.startswith("parameter-"):
-                data_[field.name] = [exp.parameters.get(field_map_[field.name], None)]
+                data_[field.name] = [exp.parameters.get(field_map_[field.name])]
             elif field.name.startswith("metric-"):
-                data_[field.name] = [exp.metrics.get(field_map_[field.name], pa.null())]
+                data_[field.name] = [exp.metrics.get(field_map_[field.name])]
             else:
                 continue
+
+        exp_test_: list[dict] = []
+        for test_ in exp.tests:
+            test_data_ = {"name": test_.name, "description": test_.description}
+            for field_name_ in test_fields:
+                if field_name_.startswith("parameter-"):
+                    test_data_[field_name_] = test_.parameters.get(
+                        test_field_map_[field_name_]
+                    )
+                elif field_name_.startswith("metric-"):
+                    test_data_[field_name_] = test_.metrics.get(
+                        test_field_map_[field_name_]
+                    )
+                else:
+                    continue
+
+            exp_test_.append(test_data_)
+
+        data_["tests"] = [exp_test_]
 
         batches.append(pa.record_batch(data_, schema=schema))
 
--- tests/test_interchange.py
+++ tests/test_interchange.py
@@ -46,6 +46,14 @@ def test_project_to_table_basic():
             "parameter-my-param": [0],
             "parameter-my-list-param": [[0, 1, 2]],
             "metric-my-metric": [0.5],
+            "tests": [
+                pa.scalar(
+                    [],
+                    type=pa.list_view(
+                        pa.struct({"name": pa.string(), "description": pa.string()})
+                    ),
+                )
+            ],
         }
     )
 
@@ -106,6 +114,133 @@ def test_project_to_table_mismatch_params():
             "parameter-my-list-param": [[0, 1, 2], None],
             "metric-my-metric": [0.5, 0.5],
             "parameter-my-features": [None, ["a", "b", "c"]],
+            "tests": [
+                pa.scalar(
+                    [],
+                    type=pa.list_view(
+                        pa.struct({"name": pa.string(), "description": pa.string()})
+                    ),
+                ),
+                pa.scalar(
+                    [],
+                    type=pa.list_view(
+                        pa.struct({"name": pa.string(), "description": pa.string()})
+                    ),
+                ),
+            ],
+        }
+    )
+
+    assert table == expected
+
+
+@time_machine.travel(
+    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")), tick=False
+)
+def test_project_to_table_no_parameters():
+    """Test converting a project to a table with no parameters and/or metrics."""
+    project = Project("project.json", author="myself", mode="w")
+    with project.log("My experiment") as exp:
+        pass
+
+    table = to_table(project)
+
+    expected = pa.table(
+        {
+            "name": ["My experiment"],
+            "slug": ["my-experiment-20250120132330"],
+            "short_slug": ["my-experiment"],
+            "author": ["myself"],
+            "created_at": [
+                pa.scalar(
+                    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")),
+                    type=pa.timestamp("s", tz="UTC"),
+                )
+            ],
+            "last_updated": [
+                pa.scalar(
+                    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")),
+                    type=pa.timestamp("s", tz="UTC"),
+                )
+            ],
+            "last_updated_by": ["myself"],
+            "tags": [pa.scalar([], type=pa.list_view(pa.string()))],
+            "tests": [
+                pa.scalar(
+                    [],
+                    type=pa.list_view(
+                        pa.struct({"name": pa.string(), "description": pa.string()})
+                    ),
+                )
+            ],
+        }
+    )
+
+    assert table == expected
+
+
+@time_machine.travel(
+    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")), tick=False
+)
+def test_project_to_table_tests():
+    """Test converting a project to a table with logged tests."""
+    project = Project("project.json", author="myself", mode="w")
+    with project.log("My experiment") as exp:
+        exp.tag("has-tests")
+        with exp.log_test(name="My test", description="My population") as test_:
+            test_.log_metric("my-metric", 0.5)
+        with exp.log_test(
+            name="My second test", description="My second population"
+        ) as test_:
+            test_.log_metric("my-metric", 0.25)
+
+    table = to_table(project)
+
+    expected = pa.table(
+        {
+            "name": ["My experiment"],
+            "slug": ["my-experiment-20250120132330"],
+            "short_slug": ["my-experiment"],
+            "author": ["myself"],
+            "created_at": [
+                pa.scalar(
+                    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")),
+                    type=pa.timestamp("s", tz="UTC"),
+                )
+            ],
+            "last_updated": [
+                pa.scalar(
+                    datetime(2025, 1, 20, 13, 23, 30, tzinfo=zoneinfo.ZoneInfo("UTC")),
+                    type=pa.timestamp("s", tz="UTC"),
+                )
+            ],
+            "last_updated_by": ["myself"],
+            "tags": [pa.scalar(["has-tests"], type=pa.list_view(pa.string()))],
+            "tests": [
+                pa.scalar(
+                    [
+                        {
+                            "name": "My test",
+                            "description": "My population",
+                            "metric-my-metric": 0.5,
+                        },
+                        {
+                            "name": "My second test",
+                            "description": "My second population",
+                            "metric-my-metric": 0.25,
+                        },
+                    ],
+                    type=pa.list_view(
+                        pa.struct(
+                            {
+                                "name": pa.string(),
+                                "description": pa.string(),
+                                "metric-my-metric": pa.float64(),
+                            }
+                        )
+                    ),
+                )
+            ],
         }
     )
 
